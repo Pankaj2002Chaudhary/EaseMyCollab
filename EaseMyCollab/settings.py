121611d71ede02ad9232c7d00d55e15ffb3a4fef
Pankaj Kumar Chaudhary <97977073+Pankaj2002Chaudhary@users.noreply.github.com>
2026-05-22 23:59:19 +0530

    port change

--- EaseMyCollab/settings.py
+++ EaseMyCollab/settings.py
@@ -94,7 +94,7 @@ CACHES = {
 # 8. EMAIL SETTINGS (OTP Feature)
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
+EMAIL_PORT = 465
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'pankajchoudhary6643@gmail.com'
 EMAIL_HOST_PASSWORD = 'tord mpul fbxx ijrk' # Pakka check karna ye App Password hai
