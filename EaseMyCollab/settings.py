9edef01e499a955d0986bb1b39a6b51af2d0a1fb
Pankaj Kumar Chaudhary <97977073+Pankaj2002Chaudhary@users.noreply.github.com>
2026-05-23 00:46:19 +0530

    Removing irrelevant email bugs

--- EaseMyCollab/settings.py
+++ EaseMyCollab/settings.py
@@ -96,10 +96,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 465
 EMAIL_USE_SSL = True
-# EMAIL_HOST_USER = 'pankajchoudhary6643@gmail.com'
-# EMAIL_HOST_PASSWORD = 'tord mpul fbxx ijrk' # Pakka check karna ye App Password hai
-EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER', 'pankajchoudhary6643@gmail.com')
-EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
+EMAIL_HOST_USER = 'pankajchoudhary6643@gmail.com'
+EMAIL_HOST_PASSWORD = 'vmww mqzx wkbh qqdx' # Pakka check karna ye App Password hai
 # 9. STATIC & MEDIA FILES
 # settings.py mein ye update karo
 STATIC_URL = '/static/'  # Shuru mein slash zaroori hai
